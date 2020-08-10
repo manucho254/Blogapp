e9962ea924034c91a22c8363fb8fe65e6b439c26
manucho254 <manuchoadero@gmail.com>
2020-08-10 22:27:58 +0300

    changes

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,23 @@
 from django.contrib import admin
-from .models import Blog
+from .models import Blog,Comment
 
 
 @admin.register(Blog)
 class PostAdmin(admin.ModelAdmin):
+
     list_display = ('entry_title', 'entry_author', 'entry_date')
     list_filter = ('entry_date', 'entry_author')
     search_fields = ('enrty_title', 'entry_text')
     raw_id_fields = ('entry_author',)
     date_hierarchy = 'entry_date'
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'created_on', 'active')
+    list_filter = ('active', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(active=True)
--- blog/urls.py
+++ blog/urls.py
@@ -6,12 +6,13 @@ from .views import (
     UpdateEntryVeiw, 
     DeleteEntryVeiw
 )
-from .views import (searchblog)
+from .views import (searchblog,post_detail)
 
 urlpatterns = [
     path("", HomeView.as_view(), name = "blog-home"),
     path('post/<int:pk>/', EntryView.as_view(), name = "post-detail"),
     path('create_entry/',  CreateEntryVeiw.as_view(), name = 'create-entry'),
+    path('/<slug:slug>', post_detail, name='post_detail'),
     path('search_blog/', searchblog, name='searchblog'),
     path('post/<int:pk>/update/', UpdateEntryVeiw.as_view(), name = "post-update"),
     path('post/<int:pk>/delete/', DeleteEntryVeiw.as_view(), name = "post-delete"),
