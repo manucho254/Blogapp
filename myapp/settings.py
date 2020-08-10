8198e975ade215f36609b23d0ef175a2692a3872
manucho254 <manuchoadero@gmail.com>
2020-08-10 23:34:50 +0300

    Procfile

--- myapp/settings.py
+++ myapp/settings.py
@@ -1,5 +1,6 @@
 import os
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -119,3 +120,6 @@ MEDIA_ROOT  = os.path.join(BASE_DIR, 'media/')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
--- myapp/wsgi.py
+++ myapp/wsgi.py
@@ -6,3 +6,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myapp.settings')
 
 
 application = get_wsgi_application()
+
+from whitenoise import WhiteNoise
+
+application = WhiteNoise(application, root='/Users\yahdeen\Downloads\Focus\manucho\myapp')
