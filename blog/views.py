1aa91b625601b4fd0ba0d68ed35417f9497d6538
manucho254 <manuchoadero@gmail.com>
2020-08-10 22:29:57 +0300

    changes

--- blog/views.py
+++ blog/views.py
@@ -76,33 +76,3 @@ def searchblog(request):
     else:
             return render(request, 'blog/index.html')
 
-
-from .models import Blog
-from .forms import CommentForm
-from django.shortcuts import render, get_object_or_404
-
-def post_detail(request, slug):
-    template_name = 'blog/comment.html'
-    post = get_object_or_404(Post, slug=slug)
-    comments = post.comments.filter(active=True)
-    new_comment = None
-    # Comment posted
-    if request.method == 'POST':
-        comment_form = CommentForm(data=request.POST)
-        if comment_form.is_valid():
-
-            # Create Comment object but don't save to database yet
-            new_comment = comment_form.save(commit=False)
-            # Assign the current post to the comment
-            new_comment.post = post
-            # Save the comment to the database
-            new_comment.save()
-    else:
-        comment_form = CommentForm()
-        context= {
-            'post': post,
-            'comments': comments,
-            'new_comment': new_comment,
-            'comment_form': comment_form}
-
-    return render(request, template_name, context)
